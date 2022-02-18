8e73efbc4e153cef1a56abc5120b43a5e5506d71
inesluparia <inesluparia@gmail.com>
2022-02-18 23:55:07 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const { response } = require("express")
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) =>{
+    res.sendFile(__dirname + "/public/index.html")
+})
+
+app.listen(8080, () => console.log("Server listening on port ", 8080))
