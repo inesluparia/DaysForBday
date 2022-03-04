39815201135905abb03e9ce9b64bc0235c55f0fa
inesluparia <inesluparia@gmail.com>
2022-03-04 08:46:18 +0100

    refactoring

--- app.js
+++ app.js
@@ -8,4 +8,4 @@ app.get("/", (req, res) =>{
     res.sendFile(__dirname + "/public/index.html")
 })
 
-app.listen(process.env.PORT || "3000", () => {console.log('Server is running!')})
+app.listen(process.env.PORT || "3000", () => {console.log('Server is running, check port 3000!')})
